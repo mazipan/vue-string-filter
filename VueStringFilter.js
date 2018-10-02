0ee21153571eb4f24e59a3bfaf919a49d5eaa5bc
Irfan Maulana <mazipanneh@gmail.com>
2018-10-02 12:57:04 +0700

    Update VueStringFilter.js

--- VueStringFilter.js
+++ VueStringFilter.js
@@ -2,29 +2,24 @@ const VueStringFilter = {
   install(Vue) {
 
     Vue.filter('lowercase', function (value) {
-
       return value.toString().toLowerCase()
     })
 
     Vue.filter('uppercase', function (value) {
-
       return value.toString().toUpperCase()
     })
 
     Vue.filter('capitalize', function (value) {
-
       return value.charAt(0).toUpperCase() + value.slice(1)
     })
 
     Vue.filter('titlecase', function (value) {
-
       return value.replace(/\w\S*/g, function (txt) {
         return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase()
       })
     })
 
     Vue.filter('slug', function (value) {
-
       // credit for gist : https://gist.github.com/mathewbyrne/1280286
       let result = value.toString()
         .toLowerCase()
